f0a4c0e22153e5d7dccd68613b33b319d9e5c102
kaaleksakhina <k.aleksahina@gmail.com>
2020-11-02 20:39:31 +0300

    Initial commit

---
+++ app/src/main/java/my/project/english/Choice.java
@@ -0,0 +1,32 @@
+package my.project.english;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Choice extends AppCompatActivity {
+    private long backPressedTime;
+    private Toast backToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exersises);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+    public void onBackPressed() {
+        try {
+            Intent intent  = new Intent(Choice.this, MainActivity.class);
+            startActivity(intent); finish();
+        }catch (Exception e) {
+
+            }
+        }
+}
---
+++ app/src/main/java/my/project/english/MainActivity.java
@@ -0,0 +1,53 @@
+package my.project.english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private long backPressedTime;
+    private Toast backToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent  = new Intent(MainActivity.this, Choice.class);
+                    startActivity(intent); finish();
+                }catch (Exception e) {
+
+                }
+            }
+        });
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    // Системная кнопка Назад
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        }
+        else {
+            backToast = Toast.makeText(getBaseContext(), "Press one more time to quit", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+}
