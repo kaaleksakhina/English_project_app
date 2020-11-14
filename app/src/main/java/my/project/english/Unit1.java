812fbebb5d5bb5c9285e79a685be198973720714
kaaleksakhina <k.aleksahina@gmail.com>
2020-11-14 20:50:45 +0300

    дата банных

--- app/src/main/java/my/project/english/Unit1.java
+++ app/src/main/java/my/project/english/Unit1.java
@@ -1,7 +1,5 @@
 package my.project.english;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
@@ -19,7 +17,6 @@ import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import joinery.DataFrame;
